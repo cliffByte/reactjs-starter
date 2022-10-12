a6662747417abd6beed9759af9e1effa6b226f0f
Saroj Dahal <isarojdahal@gmail.com>
2022-10-12 16:40:49 +0545

    optimized: atoms/CustomButton.tsx file code

--- src/ui/atoms/CustomButton.tsx
+++ src/ui/atoms/CustomButton.tsx
@@ -1,37 +1,57 @@
 import { FC, ButtonHTMLAttributes, ReactNode } from "react";
 
-
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: string;
-  size?:string;
+  size?: string;
   loading?: boolean;
   children?: ReactNode;
 }
-const CustomButton: FC<ButtonProps> = ({ variant, size,loading, children, ...rest }) => {
-  let buttonStyles = variant;
-  if (variant === "primary") buttonStyles = "primary";
-  else if (variant === "primary_outline") buttonStyles = "primary_outline";
-  else if (variant === "secondary") buttonStyles = "secondary";
-  else if (variant === "secondary_outline") buttonStyles = "secondary_outline";
-  else if (variant === "tenerary") buttonStyles = "tenerary";
-  else if (variant === "tenerary_outline") buttonStyles = "tenerary_outline";
-  else if (variant === "warning") buttonStyles = "warning";
-  else if (variant === "warning_outline") buttonStyles = "warning_outline";
-  else if (variant === "tenerary") buttonStyles = "tenerary";
-  else if (variant === "tenerary_outline") buttonStyles = "tenerary_outline";
-  else buttonStyles = "primary";
+const CustomButton: FC<ButtonProps> = ({
+  variant,
+  size,
+  loading,
+  children,
+  ...rest
+}) => {
+  const allVariants = [
+    "primary",
+    "primary_outline",
+    "secondary",
+    "secondary_outline",
+    "ternary",
+    "ternary_outline",
+    "warning",
+    "warning_outline",
+  ];
+
+  let buttonStyles = allVariants.includes(variant || "") ? variant : "primary";
 
   let buttonSize = size;
-  if (size === "small") buttonSize = "btn-small";
-  else if (size === "medium") buttonSize = "btn-medium";
-  else if (size === "large") buttonSize = "btn-large";
-  else if (size === "xl-large") buttonSize = "btn-xl-large";
-  else buttonSize = "btn-medium";
+  switch (size) {
+    case "small":
+      buttonSize = "btn-small";
+      break;
+    case "medium":
+      buttonSize = "btn-medium";
+      break;
+    case "large":
+      buttonSize = "btn-large";
+      break;
+    case "xl-large":
+      buttonSize = "btn-xl-large";
+      break;
+    default:
+      buttonSize = "btn-medium";
+  }
 
   return (
     <div className="button_section">
-      <button {...rest} className={`${buttonStyles} ${buttonSize} btn`} disabled={loading}>
-       {children}
+      <button
+        {...rest}
+        className={`${buttonStyles} ${buttonSize} btn`}
+        disabled={loading}
+      >
+        {children}
       </button>
     </div>
   );

0ce54a16eb91df723ec53ae60f14d2ef5bda9105
ibijayashah <bijayashah551@gmail.com>
2023-09-04 22:00:28 +0545

    add : folder structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from '@utils/theme'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 )
---
+++ src/utils/theme/components/Radio.ts
@@ -0,0 +1,27 @@
+import { checkboxAnatomy } from '@chakra-ui/anatomy'
+import { createMultiStyleConfigHelpers } from '@chakra-ui/react'
+import { Colors } from './../../Colors'
+
+const { definePartsStyle, defineMultiStyleConfig } = createMultiStyleConfigHelpers(checkboxAnatomy.keys)
+
+const baseStyle = definePartsStyle({
+  // define the part you're going to style
+  label: {
+    paddingTop: 1,
+    pr: 4,
+  },
+  control: {
+    height: '.7rem',
+    width: '.7rem',
+    // change the background color of the control
+    outline: `1px solid ${Colors?.primaryColor}`,
+    borderRadius: 0, // change the border radius of the control
+    _checked: {
+      // change the background color of the control when it's checked
+      bg: Colors?.primaryColor,
+      border: `1px solid ${Colors?.primaryColor}`,
+    },
+  },
+})
+
+export const RadioTheme = defineMultiStyleConfig({ baseStyle })
